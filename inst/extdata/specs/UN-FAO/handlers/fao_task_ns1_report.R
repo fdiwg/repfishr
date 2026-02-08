fbe4b7c883885aac8fe2e96edad732cefddab06a
eblondel <emmanuel.blondel1@gmail.com>
2026-02-08 17:21:15 +0100

    fao nc task

--- inst/extdata/specs/UN-FAO/handlers/fao_task_ns1_report.R
+++ inst/extdata/specs/UN-FAO/handlers/fao_task_ns1_report.R
@@ -2,7 +2,7 @@ function(sender, data, metadata, path){
   
   requireNamespace("openxlsx")
   
-  task_wb = openxlsx::buildWorkbook(list(data = data, metadata = metadata), asTable = TRUE, tableStyle = "TableStyleLight8")
+  task_wb = openxlsx::buildWorkbook(list(data = data), asTable = TRUE, tableStyle = "TableStyleLight8")
   
   #save workbook
   openxlsx::saveWorkbook(task_wb, file = path, overwrite = TRUE)
