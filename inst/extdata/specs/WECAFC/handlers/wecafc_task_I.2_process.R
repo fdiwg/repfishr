52de200ed0687222902e50851d612120a7506da3
eblondel <emmanuel.blondel1@gmail.com>
2026-02-09 14:34:49 +0100

    wecafc report - in case of no data

--- inst/extdata/specs/WECAFC/handlers/wecafc_task_I.2_process.R
+++ inst/extdata/specs/WECAFC/handlers/wecafc_task_I.2_process.R
@@ -1,51 +1,70 @@
 function(sender, data, metadata){
   
-  data_proc <- data |>
-    dplyr::mutate(
-      flagstate = sender$id, #inherit flagstate from sender
-      time_start = as.Date(time_start),
-      time_end = as.Date(time_end),
-      year = lubridate::year(time_start),
-      quarter = lubridate::quarter(time_start),
-      fishing_area = fishing_zone
-    )
+  #filter on species
+  data = data[data$species %in% fdi4R::cl_wecafc_species$code,]
   
-  # Create quarter boundaries
-  data_proc <- data_proc |>
-    dplyr::mutate(
-      quarter_start = dplyr::case_when(
-        quarter == 1 ~ as.Date(paste0(year, "-01-01")),
-        quarter == 2 ~ as.Date(paste0(year, "-04-01")),
-        quarter == 3 ~ as.Date(paste0(year, "-07-01")),
-        quarter == 4 ~ as.Date(paste0(year, "-10-01"))
-      ),
-      quarter_end = case_when(
-        quarter == 1 ~ as.Date(paste0(year, "-03-31")),
-        quarter == 2 ~ as.Date(paste0(year, "-06-30")),
-        quarter == 3 ~ as.Date(paste0(year, "-09-30")),
-        quarter == 4 ~ as.Date(paste0(year, "-12-31"))
-      ),
-      quarter_id = paste0(year, "-Q", quarter)
-    )
-
-  result <- data_proc |>
-    dplyr::group_by(quarter_id, flagstate, fishing_area, species, measurement, measurement_type, measurement_unit) |>
-    dplyr::summarise(
-      time_start = dplyr::first(quarter_start),
-      time_end = dplyr::first(quarter_end),
-      measurement_value = sum(measurement_value),
-      .groups = 'drop'
-    ) |>
-    dplyr::select(flagstate, time_start, time_end, fishing_area, species, measurement, measurement_type, measurement_value, measurement_unit)
+  #if no data
+  if(nrow(data)==0){
+    result = data.frame(
+      flagstate = character(0),
+      time_start = character(0), 
+      time_end = character(0), 
+      fishing_area = character(0), 
+      species = character(0), 
+      measurement = character(0), 
+      measurement_type = character(0), 
+      measurement_value = numeric(0), 
+      measurement_unit = character(0)
+    )  
+  }else{
   
-  #change unit to t
-  result$measurement_value = {
-    vals = units::set_units(result$measurement_value, result$measurement_unit[1], mode = "standard")
-    units(vals) <- "t"
-    as.numeric(vals)
+    data_proc <- data |>
+      dplyr::mutate(
+        flagstate = sender$id, #inherit flagstate from sender
+        time_start = as.Date(time_start),
+        time_end = as.Date(time_end),
+        year = lubridate::year(time_start),
+        quarter = lubridate::quarter(time_start),
+        fishing_area = fishing_zone
+      )
+    
+    # Create quarter boundaries
+    data_proc <- data_proc |>
+      dplyr::mutate(
+        quarter_start = dplyr::case_when(
+          quarter == 1 ~ as.Date(paste0(year, "-01-01")),
+          quarter == 2 ~ as.Date(paste0(year, "-04-01")),
+          quarter == 3 ~ as.Date(paste0(year, "-07-01")),
+          quarter == 4 ~ as.Date(paste0(year, "-10-01"))
+        ),
+        quarter_end = case_when(
+          quarter == 1 ~ as.Date(paste0(year, "-03-31")),
+          quarter == 2 ~ as.Date(paste0(year, "-06-30")),
+          quarter == 3 ~ as.Date(paste0(year, "-09-30")),
+          quarter == 4 ~ as.Date(paste0(year, "-12-31"))
+        ),
+        quarter_id = paste0(year, "-Q", quarter)
+      )
+  
+    result <- data_proc |>
+      dplyr::group_by(quarter_id, flagstate, fishing_area, species, measurement, measurement_type, measurement_unit) |>
+      dplyr::summarise(
+        time_start = dplyr::first(quarter_start),
+        time_end = dplyr::first(quarter_end),
+        measurement_value = sum(measurement_value),
+        .groups = 'drop'
+      ) |>
+      dplyr::select(flagstate, time_start, time_end, fishing_area, species, measurement, measurement_type, measurement_value, measurement_unit)
+    
+    #change unit to t
+    result$measurement_value = {
+      vals = units::set_units(result$measurement_value, result$measurement_unit[1], mode = "standard")
+      units(vals) <- "t"
+      as.numeric(vals)
+    }
+    result$measurement_unit = "t"
+    result$measurement_type = "nominal"
   }
-  result$measurement_unit = "t"
-  result$measurement_type = "nominal"
   
   #share metadata
   attr(result, "metadata") = metadata
