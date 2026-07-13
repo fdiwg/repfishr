7f49c491d5305750aa50fee18a2c67704bb32eed
eblondel <emmanuel.blondel1@gmail.com>
2026-07-13 14:44:32 +0200

    tests

--- tests/test_main.R
+++ tests/test_main.R
@@ -1,6 +1,9 @@
+require(repfishr, quietly = TRUE)
+require(testthat)
+
 testthat::test_that("repfishr",{
   
-  my_flow = reporting_flow$new(sender = "TTO", sender_type = "country")
+  my_flow = repfishr::reporting_flow$new(sender = "TTO", sender_type = "country")
 
   testthat::expect_is(my_flow$getSender(), "R6")
   testthat::expect_equal(my_flow$getSender()$name, "Trinidad and Tobago")
@@ -8,19 +11,5 @@ testthat::test_that("repfishr",{
   testthat::expect_is(my_flow$getReceiver("ICCAT"), "R6")
   testthat::expect_equal(my_flow$getReceiver("ICCAT")$name, "International Commission for the Conservation of Atlantic Tunas")
   testthat::expect_equal(length(my_flow$getReceiver("WECAFC")$getTasks()), 4L)
-  
-  data = as.data.frame( readr::read_csv("D:/Documents/CLIENTS/FAO/Projets/WECAFC-FIRMS/data/TTO-dcf-shiny/WECAFC_task_I_2_TTO_1998_2012_simplified_format.csv", guess_max = 0) )
-  data  = data[data$catch_nominal>0,]
-  data$geographic_identifier = "31.1"
-  colnames(data)[colnames(data)=="geographic_identifier"] = "fishing_area"
-  
-  output = reporting_flow$new(sender = "TTO", sender_type = "country")$
-    getReceiver("WECAFC")$
-    getTaskDefinitionById("task_I.2")$
-    process(
-      data = data, format = "simplified",
-      metadata = data.frame(author = "Fisheries Division", date = Sys.Date()),
-      path = "D:/Downloads/test_TTO.xlsx"
-    )
   
 })
